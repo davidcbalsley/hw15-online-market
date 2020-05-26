233b6fd258647d334d51605aba93af27acd2cc43
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-05-26 03:06:26 -0500

    adding wrapper to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Wrapper from "./components/Wrapper";
 import './App.css';
 
 function App() {
   return (
-    <h2>Nothing yet, but please check again soon!</h2>
+    <div>
+      <Wrapper>
+        <h2>Nothing yet, but please check again soon!</h2>
+      </Wrapper>
+    </div>
   );
 }
 
